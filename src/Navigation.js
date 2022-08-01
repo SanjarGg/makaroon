bcdbbfcc2fdf8f49a5443e824607c9ffbc403e9b
SanjarGg <xaxaskam@gmail.com>
2022-08-01 19:33:26 +0600

    sync

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./Pages/MainPage";
+import Navbar from "./Components/Navbar";
 
 function Navigation() {
   return (
     <div>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<MainPage />} />
         </Routes>
